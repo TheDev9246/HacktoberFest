bc9a6442682dd61da6672c721836b0d512481810
HackorOp <115942054+HackorOp@users.noreply.github.com>
2022-10-16 21:29:29 +0500

    TimeCounter
    TC

---
+++ TimeC.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+int main(){  
+              cin>>s;
+             s=5;
+             int h=s/3600;
+             int a=h%1;
+             int H=h-a;
+             int m=a*60;
+             int b=m%1;
+             int M=m-b;
+             int s=m*60;
+             int c=s%1;
+             int S=s-c;
+            cout<<"Hours"H<<endl;
+            cout<<"Minutes"M<<endl;
+            cout<<"Seconds"S;
+     return 0;
+  }
+              
